83b8af04d57c96bf858de761aa44b768223974af
Nik V <github@nikvdp.com>
2023-09-28 17:27:48 +0800

    luafmt

--- tests/popup_aichat.lua
+++ tests/popup_aichat.lua
@@ -1,20 +1,24 @@
-local popcorn = require'popcorn'
-local borders = require'popcorn.borders'
+local popcorn = require "popcorn"
+local borders = require "popcorn.borders"
 
 function SendToReplTerm(repl_term_chan_id, options)
-    if type(options) == 'string' then
-        options = { text_to_send = options }
+    if type(options) == "string" then
+        options = {text_to_send = options}
     end
     options = options or {}
-    local text_to_send = options.text_to_send or ''
+    local text_to_send = options.text_to_send or ""
     local use_bracketed_paste = options.use_bracketed_paste
-    if use_bracketed_paste == nil then use_bracketed_paste = true end
+    if use_bracketed_paste == nil then
+        use_bracketed_paste = true
+    end
     local add_extra_newline_to_bracketed_paste = options.add_extra_newline_to_bracketed_paste
-    if add_extra_newline_to_bracketed_paste == nil then add_extra_newline_to_bracketed_paste = true end
+    if add_extra_newline_to_bracketed_paste == nil then
+        add_extra_newline_to_bracketed_paste = true
+    end
     local use_rails_console_extra_newlines = options.use_rails_console_extra_newlines or false
 
     local to_send = text_to_send
-    if to_send == '' then
+    if to_send == "" then
         to_send = GetVisualSelection()
     end
 
@@ -24,7 +28,7 @@ function SendToReplTerm(repl_term_chan_id, options)
         local bracketed_paste_start = "\27[200~"
         local bracketed_paste_end = "\27[201~\r"
         local join_chr = use_rails_console_extra_newlines and "\r" or ""
-        to_send = { bracketed_paste_start, to_send, bracketed_paste_end }
+        to_send = {bracketed_paste_start, to_send, bracketed_paste_end}
         if add_extra_newline_to_bracketed_paste and not string.match(to_send[2], "[\n\r]$") then
             to_send[2] = to_send[2] .. "\r"
         end
@@ -32,9 +36,7 @@ function SendToReplTerm(repl_term_chan_id, options)
     end
 end
 
-
-local Job = require 'plenary.job'
-
+local Job = require "plenary.job"
 
 function Aichat(input, options)
     local output = ""
@@ -44,16 +46,21 @@ function Aichat(input, options)
     options = {
         args = options.args or {},
         timeout = options.timeout or 30000,
-        on_stdout = options.on_stdout or function(err, data) output = output .. data end,
-        on_stderr = options.on_stderr or function(err, data) stderr_output = stderr_output .. data end,
+        on_stdout = options.on_stdout or function(err, data)
+                output = output .. data
+            end,
+        on_stderr = options.on_stderr or function(err, data)
+                stderr_output = stderr_output .. data
+            end
     }
 
-    local job = Job:new {
-        command = 'aichat',
+    local job =
+        Job:new {
+        command = "aichat",
         args = options.args,
         writer = input,
         on_stdout = options.on_stdout,
-        on_stderr = options.on_stderr,
+        on_stderr = options.on_stderr
     }
 
     job:start()
@@ -61,9 +68,6 @@ function Aichat(input, options)
 
     return output
 end
-
-
-
 
 function AichatSelectedText(prompt)
     local selected_text = GetVisualSelection()
@@ -76,28 +80,26 @@ end
 function GetVisualSelection()
     -- from https://stackoverflow.com/a/47051271
     local line_start, column_start, line_end, column_end
-    if vim.fn.mode()=="v" then
+    if vim.fn.mode() == "v" then
         line_start, column_start = unpack(vim.fn.getpos("v"), 2)
         line_end, column_end = unpack(vim.fn.getpos("."), 2)
     else
         line_start, column_start = unpack(vim.fn.getpos("'<"), 2)
         line_end, column_end = unpack(vim.fn.getpos("'>"), 2)
     end
-    if (vim.fn.line2byte(line_start)+column_start) > (vim.fn.line2byte(line_end)+column_end) then
+    if (vim.fn.line2byte(line_start) + column_start) > (vim.fn.line2byte(line_end) + column_end) then
         line_start, column_start, line_end, column_end = line_end, column_end, line_start, column_start
     end
     local lines = vim.fn.getline(line_start, line_end)
     if #lines == 0 then
-        return ''
+        return ""
     end
     lines[#lines] = lines[#lines]:sub(1, column_end)
     lines[1] = lines[1]:sub(column_start)
     return table.concat(lines, "\n")
 end
 
-
-
-function entry(prompt) 
+function entry(prompt)
     local msg = "prompt was: " .. prompt .. "\n"
     msg = msg .. "selection was: " .. GetVisualSelection() .. "\n"
     open_buffer_with_text(msg)
@@ -106,23 +108,23 @@ end
 local M = {}
 
 -- Module-level variables
-M.side = 'L'
+M.side = "L"
 M.bufname = "mybuffer"
 
 -- Function to convert text to lines
 local function text_to_lines(text)
     local lines
-    if type(text) == 'string' then
-        lines = vim.split(text, '\n', true)
-    elseif type(text) == 'table' then
+    if type(text) == "string" then
+        lines = vim.split(text, "\n", true)
+    elseif type(text) == "table" then
         for i, v in ipairs(text) do
-            if type(v) ~= 'string' then
-                error('Invalid line type at index ' .. i .. ': expected string, got ' .. type(v))
+            if type(v) ~= "string" then
+                error("Invalid line type at index " .. i .. ": expected string, got " .. type(v))
             end
         end
         lines = text
     else
-        error('Invalid argument type: ' .. type(text))
+        error("Invalid argument type: " .. type(text))
     end
     return lines
 end
